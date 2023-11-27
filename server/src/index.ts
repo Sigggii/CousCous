9a5a80b67e5617ca4289026b1714320ffadba49d
siggi <simeon-holzapfel@gmx.de>
2023-11-27 22:16:45 +0100

    please heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -70,10 +70,13 @@ server.post(
   },
 );
 
-server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+server.listen(
+  { port: (process.env.PORT as number | undefined) || 8080, host: "0.0.0.0" },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  },
+);
